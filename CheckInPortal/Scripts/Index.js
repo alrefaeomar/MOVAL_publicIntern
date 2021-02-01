4d1ab8146caf1b81b46994fe8e89e8aabdb27086
Sebastian <cappadonnasebastian10@gmail.com>
2021-02-01 17:07:07 -0600

    Added animations and Deleted unnecessary files from Home folder

---
+++ CheckInPortal/Scripts/Index.js
@@ -0,0 +1,14 @@
+﻿window.sr = ScrollReveal();
+sr.reveal('.DataTable', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.CheckIn', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
